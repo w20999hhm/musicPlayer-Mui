4fe3f9c6ee9ea107ab57828f47c163389e54f0d9
w20999hhm <w20999hf@qq.com>
2016-03-30 18:41:49 +0800

    Fix Up-Menu Bug Add MusicPlayer Page

---
+++ js/player.js
@@ -0,0 +1,35 @@
+function $(s) {
+	return document.querySelectorAll(s);
+}
+
+var lis = $("#list li");
+
+for (var i = 0; i < lis.length; i++) {
+	lis[i].onclick = function() {
+		for (var j = 0; j < lis.length; j++) {
+			lis[j].className = '';
+		}
+		this.classNameame = "selected";
+		load("media/Dawn.mp3");
+	}
+}
+
+
+var xhr = new XMLHttpRequest();
+var ac = new(window.AudioContext || window.webkitAudioContext)();
+
+function load(url) {
+	xhr.open("GET", url);
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function() {
+		ac.decodeAudioData(xhr.response, function(buffer) {
+			var bufferSource = ac.createBufferSource();
+			bufferSource.buffer = buffer;
+			bufferSource.connect(ac.destination);
+			bufferSource[bufferSource.start ? "start" : "noteOn"](0);
+		}, function(err) {
+			console.log(err);
+		})
+	}
+	xhr.send();
+}
